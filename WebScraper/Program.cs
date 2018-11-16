473f73c46be9bf38582fa26ded171a6d0744feb8
Michael <mikeishere3@gmail.com>
2018-11-16 00:01:43 -0500

    initial commit

---
+++ WebScraper/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RestSharp;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using 
+
+namespace WebScraper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //https://jsonplaceholder.typicode.com/posts
+
+            var client = new RestClient("https://jsonplaceholder.typicode.com");
+            var request = new RestRequest("/users", Method.GET);
+
+            IRestResponse response = client.Execute(request);
+            var content = response.Content;
+
+            var jsonArray = JArray.FromObject(JsonConvert.DeserializeObject(content));
+
+            foreach (var user in jsonArray)
+            {
+                
+                Console.Write(user);
+
+            }
+            
+        }
+    }
+
+}
