191947a30941ad7d51049e088f40d70732a7ad12
Michael <mikeishere3@gmail.com>
2018-11-17 12:13:19 -0500

    connections established, data being added to database

--- WebScraper/Program.cs
+++ WebScraper/Program.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 using RestSharp;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
-using 
+using System.Data.SqlClient;
 
 namespace WebScraper
 {
@@ -24,13 +24,22 @@ namespace WebScraper
 
             var jsonArray = JArray.FromObject(JsonConvert.DeserializeObject(content));
 
-            foreach (var user in jsonArray)
+            using (SqlConnection conn = new SqlConnection())
             {
-                
-                Console.Write(user);
-
+                conn.ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Users;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+                conn.Open();
+                foreach (var user in jsonArray)
+                {
+                    SqlCommand insertCommand = new SqlCommand("INSERT INTO [User] (Id, name, username, email) VALUES (@id, @name, @username, @email)", conn);
+                    insertCommand.Parameters.AddWithValue("@id", user["id"].ToString());
+                    insertCommand.Parameters.AddWithValue("@name", user["name"].ToString());
+                    insertCommand.Parameters.AddWithValue("@username", user["username"].ToString());
+                    insertCommand.Parameters.AddWithValue("@email", user["email"].ToString());
+                    insertCommand.ExecuteNonQuery();
+                }
+                Console.WriteLine("Database updated");
+                conn.Close();
             }
-            
         }
     }
 
